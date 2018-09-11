7808fe1b18b6a0c6266b7cd35dfacfdaddda8685
Changxing Cao <cc5766@nyu.edu>
2018-09-11 13:50:12 -0400

    add comment

--- src/main/java/ccx/init/BlockInit.java
+++ src/main/java/ccx/init/BlockInit.java
@@ -3,6 +3,9 @@ package ccx.init;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 
+/*
+ * Here is for block init, if you want to create your customized block. Do it here.
+ */
 public class BlockInit {
 	public static final Block DIRT = Blocks.DIRT;
 }
--- src/main/java/ccx/items/ItemRegistry.java
+++ src/main/java/ccx/items/ItemRegistry.java
@@ -1,5 +1,10 @@
 package ccx.items;
 
+
+/*
+ * Here is for item init, if you want to create your customized item. Do it here.
+ */
+
 public class ItemRegistry {
 
 }
